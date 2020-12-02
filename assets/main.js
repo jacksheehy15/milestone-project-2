b604c45dd9d23f8e58e34e3bb0dc00c6672c78a0
jacksheehy15 <sheehyboi@gmail.com>
2020-12-02 21:23:07 +0000

    fixed contact page name on home page

--- assets/main.js
+++ assets/main.js
@@ -12,9 +12,8 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 51.08879542150398, lng: 4.377519937918297},
-    zoom: 9.6,
+    zoom: 12.5,
   });
 }
 
-// Back to Top button
-mybutton = document.getElementById("topbutton");
+
