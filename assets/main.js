df5e20987b07863ee49e799de88b31886990a2db
jacksheehy15 <sheehyboi@gmail.com>
2020-12-04 15:28:16 +0000

    changed pages, Dingle

--- assets/main.js
+++ assets/main.js
@@ -18,7 +18,7 @@ function myMap() {
   /** CREATES A NEW MAP INSIDE THE DIV THAT HAS THE ID  OF "map" SET
    * IT THEN CALLS THE PROPERTIES TO SEE HOW TO RENDER THE MAP */
   const map = new google.maps.Map(document.getElementById("map"), mapProp);
-  /** ADD OUR MARKERS COORDINATES AND TITLE NAME OF LOCATION AND INFORMATION */
+  /**MARKERS COORDINATES AND TITLE NAME OF LOCATION AND INFORMATION */
     const myMarks = [{"lat": 51.0883, "lng": 4.3820, "name": "De Schorre", "information": ` De Schorre is an vents venue in a former clay pit, 
                                                                         used for leisure activities, business conventions & festivals. This is where Tomorrowland festival takes
                                                                          place but you can go for a quided walk around the park in your spare time.`},
@@ -39,26 +39,25 @@ function myMap() {
                 {"lat": 51.139378146065134, "lng": 4.363397707496227, "name": "Cleydael Golf & Country Club", "information": `Cleydael Golf and Country Club is a golf club located in
                                                                          Aartselaar, Belgium and Is one of the main Golf courses in Belgium, housing an 18 hole, Par 72 course, with stunning views it is hard to not want to play a round there !`} 
 ];
-  /* Initialize an InfoObj - this will store the information displayed in each info window */
+  
   var InfoObj = [];
-  /* LOOP OVER MyMarks - INSIDE THIS LOOP WE WILL CALL OUR MARKERS */
+
   for (i = 0; i < myMarks.length; i++) {
     let contentString = '<h3>' + myMarks[i].name + '</h3>' +
     '<p>' + myMarks[i].information + '</p>' ;
-    /** I ADD BACK IN THE MARKER VARIABLE AS CALLING THE .OPEN METHOD ON OUR INFOWINDOW VARIABLE
-     *  NEEDS A REFERENCE TO THE MAP ( SET AT THE START) AND A REFERENCE TO THE MARKER PASSED IN */
+
     const marker = new google.maps.Marker({
       position: new google.maps.LatLng(myMarks[i].lat, myMarks[i].lng),
       map: map,
       title: myMarks[i].name,
       animation: google.maps.Animation.DROP,
     });
-    /** HERE WE SETUP OUR INFO WINDOW, THIS IS WHAT WE CALL  */
+
     const infowindow = new google.maps.InfoWindow({
       content: contentString,
       maxWidth: 500
     });
-    /** HERE WE ADD A CLICK LISTENER TO THE MARKER */
+    
     marker.addListener("click", function () {
       /* CLOSE OTHER WINDOWS */
       closeOtherInfo();
@@ -67,7 +66,7 @@ function myMap() {
       InfoObj[0] = infowindow;
     });
   }
-  /** This function clears out any old information   */
+  /** Clear out any old information   */
   function closeOtherInfo() {
     if( InfoObj.length > 0 ){
       InfoObj[0].set("marker", null);
@@ -76,5 +75,6 @@ function myMap() {
     }
   }
 }
-/** THIS IS JUST HOW I CALL THE MAP */
+/**Call the map */
 myMap();
+
