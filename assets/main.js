a97b0fdc1d3a54dd36cb3dcee14849b3b17a9cc3
jacksheehy15 <sheehyboi@gmail.com>
2020-12-11 22:07:12 +0000

    Links for Markers put in

--- assets/main.js
+++ assets/main.js
@@ -14,16 +14,16 @@ function myMap() {
   };
   const map = new google.maps.Map(document.getElementById("map"), mapProp);
   const myMarks =  /**Places to Stay */
-                  [{"lat": 52.13861454378943, "lng": -10.264066470990034, "name": "Seaview Heights", "information": `A little block of apartments located on John Street, Dingle.`},
-                  {"lat": 52.14156955598052, "lng":-10.268289907588573, "name": "Dingle Benners Hotel", "information": `Dingles finest 3 star hotel, located on Main Street. A superb location for tourists !`},
-                  {"lat": 52.13405592829719, "lng": -10.268530537045287 , "name": "The Dingle Skellig Hotel", "information": `Dingles most luxurious, 4 star hotel with beautiful views and incredible features.` },
-                  {"lat": 52.14125179510815, "lng":-10.286453977419042, "name": "Castlewood House", "information": `An elegant country manor and B&B , on dingles shoreline and a quick walk to town ! !`},
+                  [{"lat": 52.13861454378943, "lng": -10.264066470990034, "name": "Seaview Heights", "information": `A little block of apartments located on John Street, Dingle. <a href="https://www.seaviewheightsdingle.com/" target="_blank">Find Out More</a>`},
+                  {"lat": 52.14156955598052, "lng":-10.268289907588573, "name": "Dingle Benners Hotel", "information": `Dingles finest 3 star hotel, located on Main Street. A superb location for tourists ! <a href="https://www.dinglebenners.com/" target="_blank">Find Out More</a>`},
+                  {"lat": 52.13405592829719, "lng": -10.268530537045287 , "name": "The Dingle Skellig Hotel", "information": `Dingles most luxurious, 4 star hotel with beautiful views and incredible features.<a href="https://www.dingleskellig.com/" target="_blank">Find Out More</a>` },
+                  {"lat": 52.14125179510815, "lng":-10.286453977419042, "name": "Castlewood House", "information": `An elegant country manor and B&B , on dingles shoreline and a quick walk to town ! <a href="https://www.castlewooddingle.com/" target="_blank">Find Out More</a>`},
                 /**Places to Eat */
-                {"lat": 52.138697151519935, "lng": -10.270704749087423, "name": " The Charthouse Restaurant", "information": `A stunning restaurant with a 4.8 rating on Google, it would fail to dissapoint`},
-                  {"lat": 52.13952896495979, "lng":-10.275526380563534, "name": "Out Of The Blue Reastaurant", "information": `Dingles finest seafood restaurant on the edge of the waterline !`},
-                  {"lat": 52.14248772133324, "lng": -10.269203735457774 , "name": "The Global Village Restaurant", "information": `Dingles most luxurious 5 star restaurant on Main Street` },
+                {"lat": 52.138697151519935, "lng": -10.270704749087423, "name": " The Charthouse Restaurant", "information": `A stunning restaurant with a 4.8 rating on Google, it would fail to dissapoint <a href="https://thecharthousedingle.com/" target="_blank">Find Out More</a>`},
+                  {"lat": 52.13952896495979, "lng":-10.275526380563534, "name": "Out Of The Blue Reastaurant", "information": `Dingles finest seafood restaurant on the edge of the waterline ! <a href="https://www.outoftheblue.ie/" target="_blank">Find Out More</a>`},
+                  {"lat": 52.14248772133324, "lng": -10.269203735457774 , "name": "The Global Village Restaurant", "information": `Dingles most luxurious 5 star restaurant on Main Street <a href="https://www.globalvillagedingle.com/" target="_blank">Find Out More</a>`},
                   {"lat": 52.139899097137814, "lng":-10.271656552146398, "name": "The Anchor Down Restaurant", "information": `A lovely seafood restaurant in the heart of Dingle 
-                  town !`},
+                  town ! <a href="https://www.tripadvisor.ie/Restaurant_Review-g211861-d8490712-Reviews-Sheehy_s_Anchor_Down_Restaurant-Dingle_Dingle_Peninsula_County_Kerry.html" target="_blank">Find Out More</a>`},
                 ]; 
 
   var InfoObj = [];
